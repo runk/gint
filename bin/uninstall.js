2e38ef6bfb121992491c1c9f38c0904b35b87fec
Dmitry Shirokov <deadrunk@gmail.com>
2017-08-30 15:17:41 +1000

    Cleanup setup harness

--- bin/uninstall.js
+++ bin/uninstall.js
@@ -2,9 +2,7 @@
 
 // Run when package is uninstalled
 const path = require('path');
-const uninstallFrom = require('../lib/uninstall');
-
-console.log('Uninstalling GIT hooks');
+const uninstallFrom = require('../lib/setup/uninstall');
 
 const gintDir = path.join(__dirname, '..');
 uninstallFrom(gintDir);
